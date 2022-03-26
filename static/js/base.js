6a5c19fa30b2adf65ed6e80ba7e2f9924016dc72
Laydo <robinsonlanden@gmail.com>
2022-03-26 15:54:04 -0700

    Initial Setup
    - Initialized the project
    - Base implemented, highly inspired by APL
    - Home app started implemenatation at basic level
    - Overlay for menu navigation implemented
    - Very basic layout foundation, will evolve dramatically
    
    TODOs
    - Implement postgres DB
    - Optimize brought-in code
    - Reorganize layout and plan for scale
    - Put in skeletons for other pages/apps
    - Build out home design

---
+++ static/js/base.js
@@ -0,0 +1,64 @@
+function redirect(evt) {
+    var y = document.getElementsByClassName('active');
+    for (var i = 0; i < y.length; i++) {
+        y[i].className = '';
+    }
+    if (evt.currentTarget.id == 'laydoNavTitle') {
+        window.location.pathname = '';
+    } else {
+        window.location.pathname = evt.currentTarget.id.toLowerCase();
+        // document.getElementById(window.location.pathname).className = 'active';
+    }
+}
+
+
+function openOverlay() {
+    document.getElementById('laydoOverlay').style.display = "flex";
+    document.getElementById('laydoNavIcon').style.display = "none";
+}
+function closeOverlay() {
+    document.getElementById('laydoOverlay').style.display = "none";
+    document.getElementById('laydoNavIcon').style.display = "inherit";
+}
+
+async function getSomething() {
+
+    // let name = document.getElementById('').value;
+    let url = `${window.location.origin}/endpoint_here/?`;
+
+    fetch(url).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    }).then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.error('There has been a problem with your fetch operation: ', error);
+    })
+}
+
+// Mobile Enhancements - these are more for a side navigation menu but possibly useful for something else
+
+// document.addEventListener("click", function (e) {
+//     if (!document.getElementById("laydoNavIcon").contains(e.target) && (!document.getElementById("laydoOverlay").contains(e.target))) {
+//         closeNav();
+//     }
+// });
+
+// function handleGesture() { // props to Damjan Pavlica
+//     if ((touchendX < touchstartX) && (touchstartX - touchendX > 100)) {
+//         openNav();
+//     }
+//     if ((touchendX > touchstartX) && (touchendX - touchstartX > 100)) {
+//         closeNav();
+//     }
+// }
+
+// document.addEventListener('touchstart', e => {
+//     touchstartX = e.changedTouches[0].screenX;
+// });
+// document.addEventListener('touchend', e => {
+//     touchendX = e.changedTouches[0].screenX;
+//     handleGesture();
+// })
