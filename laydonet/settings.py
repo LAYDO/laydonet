47c1e84cde011de5192d678dc089a7d08d16483a
LAYDO <robinsonlanden@gmail.com>
2022-08-11 20:58:37 -0700

    Possible DO Config
    - Updated settings for possible DO deployment

--- laydonet/settings.py
+++ laydonet/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
+import os
+import sys
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)f!c-)t!0$53a*mnm8*@xx9kem-eu-&l8kek7)um12lax&8^n9'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY") # 'django-insecure-)f!c-)t!0$53a*mnm8*@xx9kem-eu-&l8kek7)um12lax&8^n9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True # os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",'127.0.0.1,localhost').split(',')
 
 
 # Application definition
@@ -75,17 +79,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'laydonet.wsgi.application'
 
+DEVELOPMENT_MODE = True # os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv('DATABASE_URL', None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
