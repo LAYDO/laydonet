a19656023b6070b84b8e2a96553ba0c651c2b8b2
LAYDO <robinsonlanden@gmail.com>
2022-09-05 13:40:01 -0700

    Clock Implemented
    - Clock fully implemented
    - Working on getting Periods drawn up
    - Slowly deprecating logic from weather.js

--- static/js/Clock.js
+++ static/js/Clock.js
@@ -3,28 +3,61 @@ class Clock {
     constructor(element) {
         this.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         this.radius = 0;
-        let baseW = element.clientWidth * 0.9;
-        let baseH = element.clientHeight * 0.9;
-        this.clock = document.createElementNS(Utils.svgns, 'svg');
-        this.clock.setAttribute('width', baseW + 'px');
-        this.clock.setAttribute('height', baseW + 'px');
-        this.radius = (baseW / 2) * 0.9;
-        this.circle = document.createElementNS(Utils.svgns, 'circle');
-        this.circle.setAttribute('cx', (baseW / 2).toFixed(0));
-        this.circle.setAttribute('cy', (baseW / 2).toFixed(0));
+        this.baseW = element.clientWidth;
+        this.clock = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        this.clock.setAttribute('width', this.baseW + 'px');
+        this.clock.setAttribute('height', this.baseW + 'px');
+        this.radius = (this.baseW / 2) * 0.8;
+        this.circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        this.circle.setAttribute('cx', (this.baseW / 2).toFixed(0));
+        this.circle.setAttribute('cy', (this.baseW / 2).toFixed(0));
         this.circle.setAttribute('r', this.radius.toFixed(0));
         this.circle.setAttribute('stroke', 'var(--font-color)');
         this.circle.setAttribute('fill', 'transparent');
         this.circle.setAttribute('stroke-width', '3');
-        // let circle1 = document.createElementNS(Utils.svgns, 'circle');
-        // circle1.setAttribute('cx', '100');
-        // circle1.setAttribute('cy', '100');
-        // circle1.setAttribute('r', '50');
-        // circle1.setAttribute('stroke', 'var(--font-color)');
-        // circle1.setAttribute('fill', 'transparent');
-        // circle1.setAttribute('stroke-width', '3');
+        for (let i = 0; i < this.numbers.length; i++) {
+            let hour = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            hour.setAttribute('x', '50%');
+            hour.setAttribute('y', '30');
+            hour.setAttribute('text-anchor', 'middle');
+            hour.setAttribute('fill', 'var(--font-color)');
+            hour.setAttribute('font-size', '1rem');
+            hour.textContent = this.numbers[i].toFixed(0);
+            hour.setAttribute('transform', `rotate(${(i + 1) * 30}, ${this.baseW / 2}, ${this.baseW / 2})`);
+            this.clock.append(hour);
+        }
         this.clock.append(this.circle);
         element.innerHTML = '';
         element.append(this.clock);
+        setInterval(this.updateTime.bind(this), 1000);
+    }
+    updateTime() {
+        let time = new Date();
+        let hour = time.getHours();
+        let minute = time.getMinutes();
+        let second = time.getSeconds();
+        hour = hour % 12;
+        hour = (hour * 30) + (minute / 2) + (second / 10);
+        this.drawHand(hour, 'hourHand', '4', this.baseW / 4);
+        minute = (minute * 6) + (second / 10);
+        this.drawHand(minute, 'minuteHand', '3', this.baseW / 6);
+        second = (second * 6);
+        this.drawHand(second, 'secondHand', '2', this.baseW / 9);
+    }
+    drawHand(pos, id, width, y2) {
+        let existing = document.getElementById(id);
+        if (existing) {
+            existing.remove();
+        }
+        let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        line.setAttribute('id', id);
+        line.setAttribute('stroke-width', width);
+        line.setAttribute('stroke', `${id === 'secondHand' ? 'red' : 'var(--font-color)'}`);
+        line.setAttribute('x1', `${this.baseW / 2}`);
+        line.setAttribute('y1', `${this.baseW / 2}`);
+        line.setAttribute('x2', `${this.baseW / 2}`);
+        line.setAttribute('y2', `${y2}`);
+        line.setAttribute('transform', `rotate(${pos.toFixed(0)}, ${this.baseW / 2}, ${this.baseW / 2})`);
+        this.clock.append(line);
     }
 }
--- static/js/Schedule.ts
+++ static/js/Schedule.ts
@@ -1,16 +1,23 @@
 class Schedule {
-    private schedule: Object = {};
+    private schedule: any;
     private url: string = window.location.href;
     private dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     private flip: boolean = false;
     public clockElement: HTMLElement;
+    public periodsElement: HTMLElement;
     private clock: any;
+    private periods: any;
 
     constructor(data: Object) {
         this.clockElement = document.getElementById('newClock')!;
         this.schedule = data;
         console.log(this.schedule);
         this.clock = new Clock(this.clockElement);
-        // this.clockElement.append(JSON.stringify(this.schedule));
+        this.periodsElement = document.getElementById('periods')!;
+        if (this.schedule.periods.length > 0) {
+            this.schedule.periods.forEach((period: any) => {
+                let p = new Period(period, this.periodsElement);
+            });
+        }
     }
 }
