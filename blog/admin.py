a2bdc8d4ea5acd70f9432d68491ad1671fff776d
Landen Robinson <robinsonlanden@gmail.com>
2022-10-27 16:27:33 -0700

    0.9.3.1 - First Post Styled
    - Added an embedded tweet, set up to work thru CMS
    - Styled link to GitHub
    - Will iterate and add more to Blog fields and sequential posts

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
-    search_fields = ['title', 'content']
+    search_fields = ['title', 'content1']
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
