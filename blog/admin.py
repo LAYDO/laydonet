9bfaab12bf6d52897415dbcbb8fb283181cee06a
Landen Robinson <robinsonlanden@gmail.com>
2023-01-06 18:13:53 -0800

    Blog Admin Enhance
    - Implemented SummerNote into Blog Admin
    - Should allow for more customization on blogs, namely pics
    - Need to figure out embedding tweets

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
-    search_fields = ['title', 'content1']
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
 
 admin.site.register(Post, PostAdmin)
--- laydonet/urls.py
+++ laydonet/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +33,8 @@ urlpatterns = [
     path('webapps/', include('webapps.urls')),
     path('websites/', include('websites.urls')),
     path('blog/', include('blog.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
