f0d50577751e25b44b0f3799ccd249b9b6ff2b9e
Landen Robinson <robinsonlanden@gmail.com>
2023-04-22 21:29:50 -0700

    0.4.10.2

--- fifteen_toes/consumers.py
+++ fifteen_toes/consumers.py
@@ -3,11 +3,9 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 # from fifteen_toes.models import Game
 from django.utils import timezone
 from django.apps import apps
-from .models import Game
-
 class FifteenToesConsumer(AsyncJsonWebsocketConsumer):
-    Game = apps.get_model('fifteen_toes', 'Game')
     async def connect(self):
+        Game = apps.get_model('fifteen_toes', 'Game')
         self.game_id = self.scope['url_route']['kwargs']['game_id']
         self.game_group_id = 'game_%s' % self.game_id
 
