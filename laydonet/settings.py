f7ee70ebc7d61b8c63f06bf776595d1f9bd9b225
Landen Robinson <robinsonlanden@gmail.com>
2023-04-27 21:56:16 -0700

    0.4.9.5
    - Adding laydo.net to second array for allowed hosts

--- laydonet/settings.py
+++ laydonet/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",'127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",'127.0.0.1,localhost,laydo.net').split(',')
 
 CSRF_TRUSTED_ORIGINS = ['www.laydo.net',]
 
