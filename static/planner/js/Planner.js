0f5a61b3b35663c3cdef52f1e17a5e0ea9db8771
Landen Robinson <robinsonlanden@gmail.com>
2023-08-23 12:54:17 -0500

    SVG Planner
    - Trying to convert DOM over to SVG
    - Want more control over graphics
    - Minor adjustments to resume, more to come too

--- static/planner/js/Planner.js
+++ static/planner/js/Planner.js
@@ -1,24 +1,44 @@
 "use strict";
 // Original Author: Laydo
 class Planner {
-    constructor(property) {
-        this.property = property;
-        this.property.style.height = '130rem';
-        this.property.style.width = '40rem';
+    constructor(container) {
+        this.container = container;
+        this.property = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        this.property.id = 'property';
+        this.property.setAttributeNS(null, 'height', Planner.propertyHeight + 'rem');
+        this.property.setAttributeNS(null, 'width', Planner.propertyWidth + 'rem');
         this.property.style.backgroundColor = 'green';
-        this.house = document.createElement('div');
+        // Calculate the initial left and top positions based on the parent's bounds and the given rem values
+        // const parentBounds = this.property.getBoundingClientRect();
+        // const leftPosition = parseFloat(getComputedStyle(document.documentElement).fontSize) * 6.5;
+        const topPosition = parseFloat(getComputedStyle(document.documentElement).fontSize) * 16;
+        this.house = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         this.house.id = 'house';
         this.house.classList.add('draggable');
-        this.house.style.height = '55rem';
-        this.house.style.width = '27rem';
-        this.house.style.backgroundColor = 'grey';
-        let reference = document.createElement('div');
+        this.house.setAttributeNS(null, 'height', Planner.houseHeight + 'rem');
+        this.house.setAttributeNS(null, 'width', Planner.houseWidth + 'rem');
+        this.house.setAttributeNS(null, 'fill', 'gray');
+        const centerX = (Planner.propertyWidth - Planner.houseWidth) / 2;
+        // const centerY = (Planner.propertyHeight - Planner.houseHeight) / 2;
+        this.house.setAttributeNS(null, 'x', centerX.toString());
+        this.house.setAttributeNS(null, 'y', topPosition.toString());
+        // this.house.style.top = (parentBounds.top + topPosition) + 'px';
+        // this.house.style.left = (parentBounds.left + leftPosition) + 'px';
+        let reference = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         reference.id = 'reference';
         reference.classList.add('draggable');
-        reference.style.height = '1rem';
-        reference.style.width = '1rem';
-        reference.style.backgroundColor = 'black';
+        reference.setAttributeNS(null, 'height', '1rem');
+        reference.setAttributeNS(null, 'width', '1rem');
+        reference.setAttributeNS(null, 'fill', 'black');
+        // reference.style.height = '1rem';
+        // reference.style.width = '1rem';
+        // reference.style.backgroundColor = 'black';
         this.property.appendChild(this.house);
         this.property.appendChild(reference);
+        this.container.appendChild(this.property);
     }
 }
+Planner.propertyWidth = 40;
+Planner.propertyHeight = 130;
+Planner.houseWidth = 27;
+Planner.houseHeight = 55;
--- static/planner/js/home-planner.ts
+++ static/planner/js/home-planner.ts
@@ -1,47 +1,105 @@
+// let planner: Planner;
+// let offsetY: number, offsetX: number;
+// let targetElement: HTMLElement | null = null;
+
+// init();
+
+// function init() {
+//     let property = document.getElementById('homePlanner');
+//     if (property) {
+//         planner = new Planner(property);
+//         addListeners();
+//     }
+// }
+
+// function addListeners() {
+//     document.querySelectorAll<HTMLElement>('.draggable').forEach((elem) => {
+//         let isDragging = false;
+//         let offsetX = 0;
+//         let offsetY = 0;
+
+//         elem.addEventListener('mousedown', (e) => {
+//             const mouseEvent = e as MouseEvent;
+//             isDragging = true;
+//             offsetX = mouseEvent.clientX - elem.getBoundingClientRect().left;
+//             offsetY = mouseEvent.clientY - elem.getBoundingClientRect().top;
+//         });
+
+//         window.addEventListener('mousemove', (e) => {
+//             if (!isDragging) return;
+//             const mouseEvent = e as MouseEvent;
+//             const parentRect = elem.parentElement!.getBoundingClientRect();
+//             const newX = mouseEvent.clientX - offsetX;
+//             const newY = mouseEvent.clientY - offsetY;
+
+//             const withinBoundsX = Math.max(parentRect.left, Math.min(newX, parentRect.right - elem.clientWidth));
+//             const withinBoundsY = Math.max(parentRect.top, Math.min(newY, parentRect.bottom - elem.clientHeight));
+
+//             elem.style.left = withinBoundsX + 'px';
+//             elem.style.top = withinBoundsY + 'px';
+//         });
+
+//         window.addEventListener('mouseup', () => {
+//             isDragging = false;
+//         });
+//     });
+
+// }
+// const svgNS = "http://www.w3.org/2000/svg";
+
+// // Create an SVG element
+// const svg = document.createElementNS(svgNS, "svg");
+// svg.setAttributeNS(null, "width", "800");
+// svg.setAttributeNS(null, "height", "600");
+// svg.style.border = "0.25rem solid var(--font-color)";
+
+// // Create a rectangle
+// const rect = document.createElementNS(svgNS, "rect");
+// rect.setAttributeNS(null, "x", "100");
+// rect.setAttributeNS(null, "y", "100");
+// rect.setAttributeNS(null, "width", "100");
+// rect.setAttributeNS(null, "height", "50");
+// rect.setAttributeNS(null, "fill", "#ff0000");
+
+// svg.appendChild(rect);
+let dragging = false;
+let offsetX = 0;
+let offsetY = 0;
 let planner: Planner;
-let offsetY: number, offsetX: number;
-let targetElement: HTMLElement | null = null;
-
-init();
-
-function init() {
-    let property = document.getElementById('homePlanner');
-    if (property) {
-        planner = new Planner(property);
-        addListeners();
-    }
-}
-
-function addListeners() {
-    document.querySelectorAll<HTMLElement>('.draggable').forEach((elem) => {
-        let isDragging = false;
-        let offsetX = 0;
-        let offsetY = 0;
-
-        elem.addEventListener('mousedown', (e) => {
-            const mouseEvent = e as MouseEvent;
-            isDragging = true;
-            offsetX = mouseEvent.clientX - elem.getBoundingClientRect().left;
-            offsetY = mouseEvent.clientY - elem.getBoundingClientRect().top;
-        });
-
-        window.addEventListener('mousemove', (e) => {
-            if (!isDragging) return;
-            const mouseEvent = e as MouseEvent;
-            const parentRect = elem.parentElement!.getBoundingClientRect();
-            const newX = mouseEvent.clientX - offsetX;
-            const newY = mouseEvent.clientY - offsetY;
-
-            const withinBoundsX = Math.max(parentRect.left, Math.min(newX, parentRect.right - elem.clientWidth));
-            const withinBoundsY = Math.max(parentRect.top, Math.min(newY, parentRect.bottom - elem.clientHeight));
-
-            elem.style.left = withinBoundsX + 'px';
-            elem.style.top = withinBoundsY + 'px';
-        });
-
-        window.addEventListener('mouseup', () => {
-            isDragging = false;
-        });
+let container = document.getElementById('homePlanner');
+if (container) {
+    planner = new Planner(container);
+    // Mouse down event to start dragging
+    planner.house.addEventListener("mousedown", (event) => {
+        dragging = true;
+        offsetX = event.clientX - parseFloat(planner.house.getAttributeNS(null, "x")!);
+        offsetY = event.clientY - parseFloat(planner.house.getAttributeNS(null, "y")!);
     });
-
+    
+    // Mouse move event to update the rectangle position
+    planner.property.addEventListener("mousemove", (event) => {
+        if (dragging) {
+            let x = event.clientX - offsetX;
+            let y = event.clientY - offsetY;
+    
+            // Bounds checking
+            const svgWidth = parseFloat(planner.property.getAttributeNS(null, "width")!);
+            const svgHeight = parseFloat(planner.property.getAttributeNS(null, "height")!);
+            const rectWidth = parseFloat(planner.house.getAttributeNS(null, "width")!);
+            const rectHeight = parseFloat(planner.house.getAttributeNS(null, "height")!);
+    
+            x = Math.min(Math.max(0, x), svgWidth - rectWidth);
+            y = Math.min(Math.max(0, y), svgHeight - rectHeight);
+    
+            planner.house.setAttributeNS(null, "x", x.toString());
+            planner.house.setAttributeNS(null, "y", y.toString());
+        }
+    });
+    
+    // Mouse up event to stop dragging
+    planner.property.addEventListener("mouseup", () => {
+        dragging = false;
+    });
 }
+
+
