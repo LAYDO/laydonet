0dd0189cfbed4d205b8dbc844cab951ddf08400f
Landen Robinson <robinsonlanden@gmail.com>
2024-12-12 22:18:23 -0600

    Packers and Cougs
    - Narrowed down NFL teams to just Packers
    - Implemented another widget for Cougs, extending TeamWidget for College Football
    - Endpoint to pull college football team data

--- sports/urls.py
+++ sports/urls.py
@@ -4,4 +4,5 @@ from sports import views
 urlpatterns = [
     path('', views.sports, name='sports'),
     path('nfl/<int:id>/', views.getNFLTeam, name='getNFLTeam'),
+    path('college-football/<int:id>/', views.getCollegeFootballTeam, name='getCollegeFootballTeam'),
 ]
