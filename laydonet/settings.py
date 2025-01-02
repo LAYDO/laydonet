52465941f169a6257f54c1693be03d27653cd3cc
Landen Robinson <robinsonlanden@gmail.com>
2025-01-02 02:29:24 -0600

    HOSTS Update
    - Trying to allow ai.laydo.net

--- laydonet/settings.py
+++ laydonet/settings.py
@@ -31,12 +31,14 @@ DEBUG = True  # os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "DJANGO_ALLOWED_HOSTS",
-    "127.0.0.1,localhost,*.laydo.net,https://laydonet-yztpa.ondigitalocean.app/",
+    "127.0.0.1,localhost,*.laydo.net,https://laydonet-yztpa.ondigitalocean.app/,ai.laydo.net,www.ai.laydo.net",
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.laydo.net",
     "https://laydonet-yztpa.ondigitalocean.app/",
+    "https://ai.laydo.net",
+    "https://www.ai.laydo.net",
 ]
 
 
